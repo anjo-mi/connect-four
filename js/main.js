969fe6aa94f9ffca69188b792c8d5bf67d146c4c
kyle <nojaim.k@gmail.com>
2024-11-14 15:00:48 -0500

    falling animation working and filling arrays

--- js/main.js
+++ js/main.js
@@ -1,25 +1,55 @@
 
 
-document.querySelectorAll('.column').forEach(column => {
-    column.addEventListener('click', (e) => {
-        let number = e.currentTarget.classList[0]
-        console.log(number);
-    })
-})
-
 class ConnectFour{
-    constructor(){
+    constructor(first){
         this.board = document.querySelectorAll('.column')
-        this.currentPlayer = 'red';
+        this.currentPlayer = first || 'yellow-chip';
         this.fillBoard()
+        this.colConverter = {
+            'zero': 0,
+            'one': 1,
+            'two': 2,
+            'three': 3,
+            'four': 4,
+            'five': 5,
+            'six': 6
+        }
     }
 
     fillBoard(){
+        console.log(this.board)
         this.board.forEach(column => {
             column.querySelectorAll('.cell').forEach(cell => {
                 cell.classList.remove('red-chip', 'yellow-chip')
             })
+            column.addEventListener('click', (e) => {
+                const number = this.colConverter[e.currentTarget.classList[0]]
+                console.log(number)
+                this.checkSpace(number)
+            })
         })
+        this.board = Array.from(this.board)
+    }
+
+    checkSpace(num){
+        let i = 5
+        const column = this.board[num].querySelectorAll('div')
+        console.log(column)
+
+        if (column[i].classList.contains('red-chip') || column[i].classList.contains('yellow-chip')) {
+            return
+        }else{
+            column[i].classList.add(this.currentPlayer)
+        }
+        
+        const drop = setInterval(() => {
+            if (i > 0 && !column[i-1].classList.contains('red-chip') && !column[i-1].classList.contains('yellow-chip')) {
+                column[i].classList.remove(this.currentPlayer)
+                column[--i].classList.add(this.currentPlayer)
+            }else{
+                clearInterval(drop)
+            }
+        }, 150)
     }
 }
 
